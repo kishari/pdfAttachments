0c53ebaee67b725056472034851b0b100c90b76d
Harangozó Csaba <csaba.harangozo@dbx.hu>
2011-06-24 10:26:30 +0200

    byte tömböt vár

--- src/main/java/Pilot.java
+++ src/main/java/Pilot.java
@@ -1,8 +1,7 @@
 import com.itextpdf.text.pdf.*;
+import org.apache.commons.codec.binary.Base64;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -12,36 +11,71 @@ import java.io.IOException;
  */
 public class Pilot {
 
-    public void getPdfAttachments() {
+    public static byte[] readFileToByteArray(File file) {
+		  byte[] fileAsByteArray = null;
+		  FileInputStream fis = null;
+		  try {
+			  fis = new FileInputStream(file);
+			  int numOfBytes = fis.available();
+			  fileAsByteArray = new byte[numOfBytes];
+			  fis.read(fileAsByteArray);
+		  }
+		  catch (IOException e) {
+			  e.printStackTrace();
+		  }
+		  finally {
+			  if (fis != null)
+				  try {
+					  fis.close();
+				  }
+			  	catch (IOException e) {}
+		  }
+
+		  return fileAsByteArray;
+	  }
+
+    public void getPdfAttachments(byte[] pdfBytes) {
+
+        if (pdfBytes == null) {
+            throw new RuntimeException("Pdf byte is null!");
+        }
+
+        byte[] pdf = pdfBytes;
+
+        if (Base64.isArrayByteBase64(pdfBytes)) {
+            System.out.println("Base64 jött be!");
+           pdf = Base64.decodeBase64(pdfBytes);
+        }
+
         PdfReader reader = null;
         try {
-            reader = new PdfReader("/home/csaba/Asztal/shuffle_vv/veddvelem_VV000034.pdf");
+            reader = new PdfReader(pdf);
         } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            e.printStackTrace();
         }
 
         PdfDictionary root = reader.getCatalog();
-        PdfDictionary documentnames = root.getAsDict(PdfName.NAMES);
-        PdfDictionary embeddedfiles = documentnames.getAsDict(PdfName.EMBEDDEDFILES);
-        PdfArray filespecs = embeddedfiles.getAsArray(PdfName.NAMES);
+        PdfDictionary documentNames = root.getAsDict(PdfName.NAMES);
+        PdfDictionary embeddedFiles = documentNames.getAsDict(PdfName.EMBEDDEDFILES);
+        PdfArray filespecs = embeddedFiles.getAsArray(PdfName.NAMES);
 
-        PdfDictionary filespec;
+        PdfDictionary fileSpec;
         PdfDictionary refs;
         FileOutputStream fos;
         PRStream stream;
         for (int i = 0; i < filespecs.size(); ) {
           filespecs.getAsString(i++);
-          filespec = filespecs.getAsDict(i++);
-          refs = filespec.getAsDict(PdfName.EF);
+          fileSpec = filespecs.getAsDict(i++);
+          refs = fileSpec.getAsDict(PdfName.EF);
           for (PdfName key : refs.getKeys()) {
               stream = (PRStream) PdfReader.getPdfObject(refs.getAsIndirectObject(key));
               try {
-                  fos = new FileOutputStream(String.format("/home/csaba/Asztal/shuffle_vv/%s", filespec.getAsString(key).toString()));
+                  fos = new FileOutputStream(String.format("/home/csaba/src/PdfAttachments/test/%s", fileSpec.getAsString(key).toString()));
                   fos.write(PdfReader.getStreamBytes(stream));
                   fos.flush();
                   fos.close();
               } catch (IOException e) {
-                  e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                  e.printStackTrace();
               }
           }
         }
@@ -49,6 +83,7 @@ public class Pilot {
 
     public static void main(String[] args) {
         Pilot p = new Pilot();
-        p.getPdfAttachments();
+        byte[] pdfBytes = readFileToByteArray(new File("/home/csaba/src/PdfAttachments/test/veddvelem_VV000034.pdf"));
+        p.getPdfAttachments(pdfBytes);
     }
 }
